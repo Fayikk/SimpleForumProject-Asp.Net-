130addd5339871c314fc58dfe9fac67b623e506c
Fayik <veznedaroglufayik2@gmail.com>
2022-10-14 11:48:04 +0300

    Add project files.

---
+++ JokesWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JokesWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ JokesWeb/Models/Joke.cs
@@ -0,0 +1,14 @@
+﻿namespace JokesWeb.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
